5b8427fde58217f9b418489bc8222c9e99d635e3
Aurangzaib <aurangzaib@macbooks-MacBook-Pro.local>
2019-08-22 18:35:52 +0500

    download image library in progress

--- MindValleyiOSChallenge/MindValleyiOSChallenge/CustomCells/AdsCollectionViewCell/AdsCollectionViewCell.swift
+++ MindValleyiOSChallenge/MindValleyiOSChallenge/CustomCells/AdsCollectionViewCell/AdsCollectionViewCell.swift
@@ -14,6 +14,7 @@ class AdsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imgVwAds: UIImageView!
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var lblSubTitle: UILabel!
+    @IBOutlet weak var btnDownloadImage: UIButton!
     
     var isAnimated = false
 
